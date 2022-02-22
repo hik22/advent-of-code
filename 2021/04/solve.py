318044a9a977ac29622cc68694e8a2978bad61a1
hik <wcho.streaming@gmail.com>
2022-02-23 01:16:38 +0900

    solve day 04 of 2021 with python

---
+++ 2021/04/solve.py
@@ -0,0 +1,116 @@
+import sys
+if len(sys.argv) != 2:
+    print('Usage: python solve.py <input-file-name>')
+    sys.exit(1)
+
+fname = sys.argv[1]
+input = [line.strip() for line in open(fname)]
+
+# part 1
+
+# get numbers
+nums = [int(str) for str in input[0].split(',')]
+
+# get boards
+input = input[2:]
+boards = []
+board = [] # buffer
+for line in input:
+    if line == '':
+        boards.append(board)
+        board = []
+    else:
+        board_row = [int(str) for str in line.split()]
+        board.append(board_row)
+boards.append(board)
+
+# initialize marked positions for each board
+marked_all = []
+for _ in range(len(boards)):
+    marked_all.append([[False for _ in range(5)] for _ in range(5)])
+
+def mark(num, board, marked):
+    # mark number
+    for i in range(len(board)): # for each row
+        if num in board[i]:
+            j = board[i].index(num)
+            marked[i][j] = True
+            return True
+
+    return False # marking failed
+
+def is_row_bingo(marked):
+    for i in range(5):
+        if marked[i].count(True) == 5:
+            return True
+    return False
+
+def is_col_bingo(marked):
+    for i in range(5): # column index i
+        is_bingo = True
+        for j in range(5): # row index j
+            if not marked[j][i]:
+                is_bingo = False
+                break
+
+        if is_bingo:
+            return True
+    return False
+
+def get_sum_of_unmarked(board, marked):
+    sum = 0
+    for i in range(5): # row
+        for j in range(5): # column
+            if not marked[i][j]:
+                sum += board[i][j]
+    return sum
+
+result = None
+for num in nums:
+    is_bingo = False
+
+    # mark number for each board
+    for i in range(len(boards)):
+        mark(num, boards[i], marked_all[i])
+
+    # check bingo
+    for i in range(len(boards)): # for i-th board
+        if is_row_bingo(marked_all[i]) or is_col_bingo(marked_all[i]):
+            sum = get_sum_of_unmarked(boards[i], marked_all[i])
+            result = sum * num
+            is_bingo = True
+            break
+
+    if is_bingo:
+        break
+
+print('Part 1:', result)
+
+# part 2
+
+# initialize marked positions for each board
+marked_all = []
+for _ in range(len(boards)):
+    marked_all.append([[False for _ in range(5)] for _ in range(5)])
+
+playing = [i for i in range(len(boards))]
+
+result = None
+for num in nums:
+    # mark number for each board
+    for i in playing:
+        mark(num, boards[i], marked_all[i])
+
+    # check bingo
+    for i in playing:
+        if is_row_bingo(marked_all[i]) or is_col_bingo(marked_all[i]):
+            playing.remove(i)
+            if len(playing) == 0: # last board
+                sum = get_sum_of_unmarked(boards[i], marked_all[i])
+                result = sum * num
+                break
+
+    if len(playing) == 0:
+        break
+
+print('Part 2:', result)
